12e0cfa0c3afac76fb42801c04d7659b3085bbc6
hilmansulaeman <hilmansulaeman0504@gmail.com>
2022-10-17 20:11:26 +0700

    Video #4 - Blade Templating Engine

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -1,18 +1,10 @@
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Blog | About</title>
-</head>
+@section('container')
+  <h1>Halaman About</h1>
+  <h3>{{ $name }}</h3>
+  <p>{{ $email }}</p>
+  <img src="img/{{ $image; }}" alt="{{ $name }}" width="150">
+@endsection
 
-<body>
-  <h1>Halaman Abour</h1>
-  <h3><?= $name; ?></h3>
-  <p><?= $email; ?></p>
-  <img src="img/<?= $image; ?>" alt="<? $name ?>" width="150">
-</body>
 
-</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Hilman Sulaeman",
         "email" => "hilmansulaeman0504@gmail.com",
         "image" => "foto.jpg"
@@ -27,6 +30,42 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/posts', function () {
-    return view('posts');
+
+Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "Judul Post Pertama",
+            "autor" => "Hilman Sulaeman",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//halaman  
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "Judul Post Pertama",
+            "autor" => "Hilman Sulaeman",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ]
+    ];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
