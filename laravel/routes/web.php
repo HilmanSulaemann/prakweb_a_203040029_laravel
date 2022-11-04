38a1551d3f1104bcf35509eceb8c71f0bc7944ab
hilmansulaeman <hilmansulaeman0504@gmail.com>
2022-11-04 17:53:57 +0700

    Menyelesaikan Video #17

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,15 +2,15 @@
 
 
 use App\Models\Category;
+use GuzzleHttp\Middleware;
 use PhpParser\Node\Stmt\Return_;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use function PHPUnit\Framework\returnValue;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
-use GuzzleHttp\Middleware;
+use App\Http\Controllers\DashboardPostController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 
@@ -81,4 +81,7 @@ Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
